9ac69f09f9f27736825229d7c6cbe98a05752d94
Roshan Singh <singh.roshan9718@gmail.com>
2024-04-26 15:34:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Header() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='header'>
+
+      <div className='logo-container'>
+        <img className='logo' src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" alt="Logo" />
+      </div>
+
+      <div className='nav-items'>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+
     </div>
+
   );
 }
 
-export default App;
+export default Header;
+
